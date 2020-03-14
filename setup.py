1c43b3175ce0ac4b41381979e54e64e1488bd24f
Marco Esposito <marco.esposito@tum.de>
2020-03-15 00:10:33 +0100

    Linted catkin files

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['tf_bag'],
-    #scripts=['bin/myscript'],
     package_dir={'': 'src'}
 )
 
