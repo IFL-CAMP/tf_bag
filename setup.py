72d0c9abc5a795a45008e9ed32f2c14d0213b255
Marco Esposito <esposito@imfusion.com>
2022-12-20 14:57:53 +0100

    Converted to tf2 and ROS2

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
-    packages=['tf_bag'],
-    package_dir={'': 'src'}
+package_name = 'tf_bag'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Marco Esposito',
+    maintainer_email='esposito@imfusion.com',
+    description='Utilities to transparently use tf data recorded with rosbag',
+    license='LGPLv3',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
 )
-
-setup(**d)
---
+++ tf_bag/__init__.py
@@ -0,0 +1,1 @@
+from .BagTfTransformer import BagTfTransformer
